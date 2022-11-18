c2b19cbc9980b0867c567a298e2c211d5bc0cf29
rubences <91731294+rubences@users.noreply.github.com>
2022-11-18 17:09:24 +0000

    cambios

--- adaptador.py
+++ adaptador.py
@@ -1,6 +1,6 @@
 #patrones de uso de la ZCA, dos interfaces y dos objetos
 #que implementan las interfaces
-from zope.interface import Interface, implements
+''' from zope.interface import Interface, implements
 class IAdaptador(Interface):
     def adaptar():
         pass
@@ -12,3 +12,34 @@ class Adaptado(object):
     pass
 class Adaptado2(object):
     pass
+'''
+
+from zope.interface import Interface, Attribute,  implements
+
+class IPerro(Interface):
+    nombre = Attribute("""Nombre del perro""")
+    def ladrar(filename):
+        """Método que permite hacerlo ladrar"""
+
+class Perro(object):
+    implements(IPerro)
+    nombre = 'u'  
+    def __init__(self, nombre):
+        self.nombre = nombre
+    def ladrar(self): 
+        """Método que permite hacerlo ladrar"""
+        print('Guau')   
+
+class IGato(Interface):   
+    nombre = Attribute("""Nombre del gato""")   
+    def maullar(filename):   
+        """Método que permite hacerlo maullar"""   
+
+class Gato(object):   
+    implements(IGato)   
+    nombre = u''   
+    def __init__(self, nombre):   
+        self.nombre = nombre   
+    def maullar(self):   
+        """Método que permite hacerlo maullar"""  
+        print('Miau')   
